9be4cb359089fe0d862c177c9892fbe193f909ee
Ulugbek <ulugb3k270@gmail.com>
2022-07-16 13:13:38 +0500

    Banner finished

---
+++ client/src/Components/Banner/Banner.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "./banner.scss";
+import "./media.banner.scss";
+
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="banner__container container">
+        <div className="banner__left">
+          <h2>
+            Har joyda, istalgan vaqtda dasturlashni o’rganing. <br />
+            <span> Bepul dasturlash kurslari </span>
+            orqali o’z sayohatingizni boshlang
+          </h2>
+        </div>
+        <div className="banner__right">
+          <button>
+            <BsFillArrowRightCircleFill /> Boshlash
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
