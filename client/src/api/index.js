4891f79da7b2e2bece9400ebb61000c4c640f739
Ulugbek <ulugb3k270@gmail.com>
2022-07-17 13:06:16 +0500

    added server api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:9000" });
+// const API = axios.create({ baseURL: "http://localhost:9000" });
+const API = axios.create({ baseURL: "https://mohirdev-clone-server.herokuapp.com/" });
 
 export const getAllCourses = () => API.get(`/courses/get-all`);
