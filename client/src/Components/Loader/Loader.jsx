689e79a9ccb2997f7539a2b7bdb60b52633c224d
Ulugbek <ulugb3k270@gmail.com>
2022-07-17 12:21:58 +0500

    Finished Loader Component

---
+++ client/src/Components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./loader.scss";
+const Loader = () => {
+  return (
+    <div className="loader">
+      <span className="loader-icon"></span>
+    </div>
+  );
+};
+
+export default Loader;
