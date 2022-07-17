af271176688fd3d34ff89a80bd48c319a2f36bc5
Ulugbek <ulugb3k270@gmail.com>
2022-07-17 13:36:42 +0500

    Caution Component added

--- client/src/Components/Caution/Caution.jsx
+++ client/src/Components/Caution/Caution.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./caution.scss";
+import "./media.caution.scss";
 const Caution = () => {
   return (
     <div className="caution">
       <p>
         Bu Mohirdev saytining clone versiyasi. Mohirdev saytiga kirish uchun
         quyidagi havolaga o'ting{" "}
-        <a href="https://mohirdev.uz" rel="noreferrer" target="_blank">Mohirdev saytiga o'tish</a>
+        <a href="https://mohirdev.uz" rel="noreferrer" target="_blank">
+          Mohirdev saytiga o'tish
+        </a>
       </p>
     </div>
   );
