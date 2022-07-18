b0970accf90f682f46e355f9bf968cfa5cb2d162
Ulugbek <ulugb3k270@gmail.com>
2022-07-18 07:43:40 +0500

    fixed caution

--- client/src/Components/Caution/Caution.jsx
+++ client/src/Components/Caution/Caution.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./caution.scss";
 import "./media.caution.scss";
+
 const Caution = () => {
+  const [activeClass, setActiveClass] = useState("active");
+
   return (
-    <div className="caution">
+    <div className={`caution ${activeClass}`}>
       <p>
         Bu Mohirdev saytining clone versiyasi. Mohirdev saytiga kirish uchun
         quyidagi havolaga o'ting{" "}
@@ -11,6 +14,10 @@ const Caution = () => {
           Mohirdev saytiga o'tish
         </a>
       </p>
+
+      <button className="caution__button" onClick={() => setActiveClass("")}>
+        Yopish
+      </button>
     </div>
   );
 };
